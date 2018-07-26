f57c6e79a4abefcabc1b01a535e3faf54ad46e4a
wileybaba <wileymckayconte@gmail.com>
2018-07-25 20:58:07 -0600

    hotfix_take_2

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module Cropshare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = false
+
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
